c990665a372591fe1fd64030a7a64a4c200aac06
akshay kashyap <akshaykashyap.2013@gmail.comgit config -- global user.name akashyap2013>
2022-09-01 18:23:46 +0530

    Basic Routes Added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-        <h1>Auth App</h1>
-    </div>
+    <Routes>
+      <Route path='/' element={<div>Home</div>}></Route>
+      <Route path='/login' element={<div>Login</div>}></Route>
+      <Route path='/dashboard' element={<div>Dashboard</div>}></Route>
+      <Route path='/settings' element={<div>Settings</div>}></Route>
+    </Routes>
   );
 }
 
